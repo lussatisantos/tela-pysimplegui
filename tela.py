d0aa70c53f21e8df848334db2185ae504fa0026c
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-10-14 12:24:59 +0100

    output

--- tela.py
+++ tela.py
@@ -12,28 +12,33 @@ class TelaPython:
             [sg.Checkbox('Gmail', key='gmail'), sg.Checkbox('Outlook', key='outlook'), sg.Checkbox('Yahoo', key='yahoo')],
             [sg.Text('Aceita Cartao')],
             [sg.Radio('Sim', 'cartoes', key='aceitaCartao'), sg.Radio('Nao', 'cartoes', key='naoAceitaCartao')],
-            [sg.Button('Enviar dados')]
+            [sg.Button('Enviar dados')],
+            [sg.Output(size=(30, 20))]
         ]
         # janela
-        janela = sg.Window('Dados do Usuario', ).layout(layout)
+        self.janela = sg.Window('Dados do Usuario', ).layout(layout)
         # extrair dados da tela
-        self.button, self.values = janela.Read()
+        self.button, self.values = self.janela.Read()
     
     def Iniciar(self):
-        nome = self.values['nome']
-        idade = self.values['idade']
-        aceita_gmail = self.values['gmail']
-        aceita_outlook = self.values['outlook']
-        aceita_yahoo = self.values['yahoo']
-        aceita_cartao = self.values['aceitaCartao']
-        nao_aceita_cartao = self.values['naoAceitaCartao']
-        print(f'nome: {nome}')
-        print(f'idade: {idade}')
-        print(f'aceita gmail: {aceita_gmail}')
-        print(f'aceita outlook: {aceita_outlook}')
-        print(f'aceita yahoo: {aceita_yahoo}')
-        print(f'Aceita cartao: {aceita_cartao}')
-        print(f'Nao aceita cartao: {nao_aceita_cartao}')
+        while True:
+            # extrair dados da tela
+            self.button, self.values = self.janela.Read()
+
+            nome = self.values['nome']
+            idade = self.values['idade']
+            aceita_gmail = self.values['gmail']
+            aceita_outlook = self.values['outlook']
+            aceita_yahoo = self.values['yahoo']
+            aceita_cartao = self.values['aceitaCartao']
+            nao_aceita_cartao = self.values['naoAceitaCartao']
+            print(f'nome: {nome}')
+            print(f'idade: {idade}')
+            print(f'aceita gmail: {aceita_gmail}')
+            print(f'aceita outlook: {aceita_outlook}')
+            print(f'aceita yahoo: {aceita_yahoo}')
+            print(f'Aceita cartao: {aceita_cartao}')
+            print(f'Nao aceita cartao: {nao_aceita_cartao}')
 
 tela = TelaPython()
 tela.Iniciar()
