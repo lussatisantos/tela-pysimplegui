e5e57eae13f666921b4bd3ae33d430efe8d5c82d
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-10-14 12:19:52 +0100

    radius e enviar dados via script

--- tela.py
+++ tela.py
@@ -10,6 +10,8 @@ class TelaPython:
             [sg.Text('idade', size=(5,0)),sg.Input(size=(15,0), key='idade')],
             [sg.Text('Quais provedores de email sao aceitos?')],
             [sg.Checkbox('Gmail', key='gmail'), sg.Checkbox('Outlook', key='outlook'), sg.Checkbox('Yahoo', key='yahoo')],
+            [sg.Text('Aceita Cartao')],
+            [sg.Radio('Sim', 'cartoes', key='aceitaCartao'), sg.Radio('Nao', 'cartoes', key='naoAceitaCartao')],
             [sg.Button('Enviar dados')]
         ]
         # janela
@@ -23,11 +25,15 @@ class TelaPython:
         aceita_gmail = self.values['gmail']
         aceita_outlook = self.values['outlook']
         aceita_yahoo = self.values['yahoo']
+        aceita_cartao = self.values['aceitaCartao']
+        nao_aceita_cartao = self.values['naoAceitaCartao']
         print(f'nome: {nome}')
         print(f'idade: {idade}')
         print(f'aceita gmail: {aceita_gmail}')
         print(f'aceita outlook: {aceita_outlook}')
         print(f'aceita yahoo: {aceita_yahoo}')
+        print(f'Aceita cartao: {aceita_cartao}')
+        print(f'Nao aceita cartao: {nao_aceita_cartao}')
 
 tela = TelaPython()
 tela.Iniciar()
