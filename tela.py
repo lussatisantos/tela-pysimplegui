1c87c414c1afd9b62b106496a1f0cbe675032ef6
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-10-14 12:30:53 +0100

    slider
    testando a velocidade do script

--- tela.py
+++ tela.py
@@ -12,6 +12,7 @@ class TelaPython:
             [sg.Checkbox('Gmail', key='gmail'), sg.Checkbox('Outlook', key='outlook'), sg.Checkbox('Yahoo', key='yahoo')],
             [sg.Text('Aceita Cartao')],
             [sg.Radio('Sim', 'cartoes', key='aceitaCartao'), sg.Radio('Nao', 'cartoes', key='naoAceitaCartao')],
+            [sg.Slider(range=(0, 255), default_value=0, orientation='h', size=(15, 20), key='slidervelocidade')],
             [sg.Button('Enviar dados')],
             [sg.Output(size=(30, 20))]
         ]
@@ -32,6 +33,7 @@ class TelaPython:
             aceita_yahoo = self.values['yahoo']
             aceita_cartao = self.values['aceitaCartao']
             nao_aceita_cartao = self.values['naoAceitaCartao']
+            velocidade_script = self.values['slidervelocidade']
             print(f'nome: {nome}')
             print(f'idade: {idade}')
             print(f'aceita gmail: {aceita_gmail}')
@@ -39,6 +41,7 @@ class TelaPython:
             print(f'aceita yahoo: {aceita_yahoo}')
             print(f'Aceita cartao: {aceita_cartao}')
             print(f'Nao aceita cartao: {nao_aceita_cartao}')
+            print(f'Velocidade script: {velocidade_script}')
 
 tela = TelaPython()
 tela.Iniciar()
